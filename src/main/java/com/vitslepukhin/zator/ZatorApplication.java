17cbba9b8a3ead77c1edc826fde8d912b3567ef3
Vitalii Slepukhin <vitslepukhin@gmail.com>
2023-09-29 20:21:00 +0300

    Start project

---
+++ src/main/java/com/vitslepukhin/zator/ZatorApplication.java
@@ -0,0 +1,13 @@
+package com.vitslepukhin.zator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitslepukhin/zator/ZatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitslepukhin.zator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
